3c46de9765f12a8cd0a1f5386e51fdca0d152cfe
Yara Elmalah <yaraelmalah2016@gmail.com>
2020-07-12 14:05:32 +0200

    Done with tabs Section

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,7 @@ $(function() {
     //Deal with Tabs
     $('.tab-switch li').on('click', function() {
         $(this).addClass('selected').siblings().removeClass('selected');
+        $($(this).data('class')).siblings().hide().end().fadeIn(300);
     })
     var myNav = $('.navbar');
     $(window).on('scroll', function() {
